e500054781addf6354ec5ce2214239ad38e90b33
Michael Vann <michaelvann304@yahoo.com>
2023-07-11 23:08:57 -0500

    created the 'console.rs' module

---
+++ src/console.rs
@@ -0,0 +1,15 @@
+use std::io::{Write, stdout, stdin};
+
+pub fn read_string(preceding: Option<&str>) -> std::io::Result<String> {
+    if let Some(preceding) = preceding {
+        stdout().write(
+            format!("{}: ", preceding).as_bytes()
+        )?;
+        stdout().flush()?;
+    }
+
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    let input = buffer.trim();
+    Ok(input.to_string())
+}

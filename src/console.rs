e5db351812e3f270531fc2815520e1c3c49835cb
Michael Vann <michaelvann304@yahoo.com>
2023-07-13 21:23:18 -0500

    added clear_screen function

--- src/console.rs
+++ src/console.rs
@@ -1,8 +1,12 @@
-use std::io::{Write, stdout, stdin};
+use {
+    std::io::{Write, stdout, stdin},
+    std::process::Command,
+    std::env,
+};
 
 
-/// Reads a line of input from the stdin stream and returns it
-/// The 'preceding' string is what will be printed *immediately* before reading a line
+/// Reads a line of input from the stdin stream and returns it.
+/// The 'preceding' string is what will be printed *immediately* before reading a line.
 pub fn read_line(preceding: Option<&str>) -> std::io::Result<String> {
     if let Some(preceding) = preceding {
         stdout().write(
@@ -15,4 +19,14 @@ pub fn read_line(preceding: Option<&str>) -> std::io::Result<String> {
     stdin().read_line(&mut buffer)?;
     let input = buffer.trim();
     Ok(input.to_string())
+}
+
+pub fn clear_screen() -> std::io::Result<()> {
+    if env::consts::OS == "windows" {
+        Command::new("cls").spawn()?;
+    }
+    else if env::consts::OS == "linux" {
+        Command::new("clear").spawn()?;
+    }
+    Ok(())
 }
